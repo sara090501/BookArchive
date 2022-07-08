8f1b840e8944d7bc29caa4d672dc24128eae3ddb
sara090501 <geratova.sara@gmail.com>
2022-07-08 15:04:36 +0200

    upgrade many-to-many relationship

--- src/main/java/com/example/demo/entities/System.java
+++ src/main/java/com/example/demo/entities/System.java
@@ -1,12 +1,20 @@
 package com.example.demo.entities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class System {
     @Id
     private Integer column;
-    //private Author author = new Author();
-    //private Book book = new Book();
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
